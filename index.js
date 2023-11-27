7f40549886ec7d601d61e9d9cf31e9bba8582f82
Schadde-Ronny <frevert71@gmail.com>
2023-11-27 02:34:23 +0100

    Responsiv Design begonnen

--- index.js
+++ index.js
@@ -1,45 +1,25 @@
-
-        const toggleCheckbox = document.getElementById('toggle');
-        const colorPicker = document.getElementById('colorPicker');
-        const body = document.body;
-
-        toggleCheckbox.addEventListener('change', function () {
-            if (toggleCheckbox.checked) {
-                colorPicker.style.display = 'block';
-            } else {
-                colorPicker.style.display = 'none';
-                body.style.backgroundColor = ''; 
-            }
-        });
-
-        colorPicker.addEventListener('input', function () {
-            body.style.backgroundColor = colorPicker.value;
-        });
-        function redirectToDrWait() {
-            window.location.href = 'DrWait.html'
-        }
-
-        function redirectToTermine() {
-            window.location.href = 'Termine.html'
-        }
-        function redirectToRezepte() {
-            window.location.href = 'Rezepte.html'
-        }
-        function redirectToPatienten() {
-            window.location.href = 'Patienten.html'
-        }
-        function redirectToJobs() {
-            window.location.href = 'Jobs.html'
-        }
-        function redirectToLeistung() {
-            window.location.href = 'Leistungsspektrum.html'
-        }
-        function redirectToFacharzt() {
-            window.location.href = 'Facharzt.html'
-        }
-        function redirectToMasern() {
-            window.location.href = 'Masern.html'
-        }
-        function redirectToBilder() {
-            window.location.href = 'Bilder.html'
-        }
+// Definieren Sie das pageMapping-Objekt zuerst
+const pageMapping = {
+    'button1': 'Notfall.html',
+    'button2': 'Termine.html',
+    // 'button3': 'Rezepte.html',
+    // 'button4': 'Patienten.html',
+    // 'button5': 'Jobs.html',
+    // 'button6': 'Leistungsspektrum.html',
+    // 'button7': 'Facharzt.html',
+    // 'button8': 'Impfung.html',
+    // 'button9': 'Bilder.html'
+  };
+  
+  // Verwenden Sie die Funktion redirectToBilder() mit dem korrekten Parameter
+  function redirectToPage(buttonId) {
+    const pageUrl = pageMapping[buttonId];
+    console.log('buttonId:', buttonId);
+    console.log('pageUrl:', pageUrl);
+    if (pageUrl) {
+      window.location.href = pageUrl;
+    } else {
+      const buttonText = document.getElementById(buttonId).innerText;
+      alert(`Leider ist diese Seite für "${buttonText}" in Bearbeitung. Bitte versuchen Sie es später noch einmal.`);
+    }
+  }
